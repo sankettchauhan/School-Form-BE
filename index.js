99ee4c094cd324b45791a4584b7bbfc512d0467a
sankettchauhan <sanket.chauhan4@gmail.com>
2022-01-09 17:25:38 +0530

    initial commit, form model added

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("config");
+const forms = require("./routes/forms");
+
+mongoose.connect(config.get("db"), {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+// Check connection
+db.once("open", () => "Connected to MongoDB");
+
+// Check for errors
+db.on("error", (err) => console.log(err));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("server is running..");
+});
+
+app.use("/api/form", forms);
+
+app.listen(3000, () => {
+  console.log(`server stated at http://localhost:3000/..`);
+});
---
+++ routes/forms.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const router=express.Router()
+const {Form}=require('../models/form')
+
+router.post("/")
+
+export default function (req, res) {
+  // const {error}=validateForm(req.body)
+  // if(error) return res.status(400).send(error.details[0].message)
+});
+
+module.exports=router;
